d31bd2a1966fb35d57c13d6aa0fffc2ec22e6c63
sydney-runkle <sydneymarierunkle@gmail.com>
2021-06-21 13:43:26 -0400

    modifying how display names are pulled based on service name

---
+++ dashboard_supplements/demo_text/display_names.py
@@ -0,0 +1,71 @@
+"""Personas, Business names, and properties for display on each page."""
+from typing import List
+
+pfr_personas: List[str] = [
+    "Amy Nye",
+    "Arvinder Singh",
+    "Allison Hull",
+    "Lauren Daniels",
+    "Ramon Martinez",
+    "Randall Pearson",
+]
+
+life_events_personas: List[str] = [
+    "Amy Nye",
+    "Arvinder Singh",
+    "Allison Hull",
+    "Lauren Daniels",
+    "Ramon Martinez",
+    "Randall Pearson",
+]
+
+auto_prefill_personas: List[str] = [
+    "Amy Nye",
+    "Arvinder Singh",
+    "Allison Hull",
+    "Lauren Daniels",
+    "Ramon Martinez",
+    "Randall Pearson",
+]
+
+life_prefill_personas: List[str] = [
+    "Amy Nye",
+    "Arvinder Singh",
+    "Allison Hull",
+    "Lauren Daniels",
+    "Ramon Martinez",
+    "Randall Pearson",
+]
+
+smb_names: List[str] = [
+    "Smith Bros Moving Co",
+    "Small Town Gazette",
+    "Little Shop of Flowers",
+    "Wobbly Wheel Tire and Alignment",
+    "Cherry on Top Cupcake",
+    "Uptown Accountants LLC",
+]
+
+persona_names_label_mapper = {
+    "Amy Nye": "Amy Nye",
+    "Arvinder Singh": "Arvinder Singh",
+    "Allison Hull": "Hull Family",
+    "Lauren Daniels": "Lauren Daniels",
+    "Ramon Martinez": "Martinez Family",
+    "Randall Pearson": "Randall Pearson",
+}
+
+business_label_mapper: dict = {
+    "Super Smart Software": "Super Smart Software",
+    "Main Street Cafe": "Main Street Cafe",
+    "Smith Bros Moving Co": "Smith Bros Moving Co",
+    "Small Town Gazette": "Small Town Gazette",
+    "NY Deli Bagels": "NY Deli Bagels",
+    "Little Shop of Flowers": "Little Shop of Flowers",
+    "Wobbly Wheel Tire and Alignment": "Wobbly Wheel Tire and Alignment",
+    "Spot and Mittens Veterinary Clinic": "Spot and Mittens Veterinary Clinic",
+    "Cherry on Top Cupcake": "Cherry on Top Cupcake",
+    "Uptown Accountants LLC": "Uptown Accountants, PLLC",
+    "Stratford Inn Bed & Breakfast": "Stratford Inn Bed & Breakfast",
+}
+property_label_mapper: dict = {}
