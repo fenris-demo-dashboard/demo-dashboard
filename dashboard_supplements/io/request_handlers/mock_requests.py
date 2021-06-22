cbb193120729b6da3fb79364a521de555a684262
sydney-runkle <sydneymarierunkle@gmail.com>
2021-06-22 11:52:48 -0400

    changing mock addresses to most recent for new movers

--- dashboard_supplements/io/request_handlers/mock_requests.py
+++ dashboard_supplements/io/request_handlers/mock_requests.py
@@ -22,7 +22,7 @@ def mock_personal_query(
     name_match_condition = (
         fake_data_df["requestBody.person.firstName"].str.strip() == first_name
     ) & (fake_data_df["requestBody.person.lastName"].str.strip() == last_name)
-    persona_row_match = fake_data_df.loc[name_match_condition].iloc[0]
+    persona_row_match = fake_data_df.loc[name_match_condition].iloc[-1]
     response_json = json.loads(persona_row_match.to_json())
     return dict(response_json)
 
@@ -42,7 +42,7 @@ def mock_property_query(
     address_match_condition = (
         fake_data_df["requestBody.address.addressLine1"].str.strip() == address_line1
     ) & (fake_data_df["requestBody.address.zipCode"].str.strip() == zip_code)
-    address_row_match = fake_data_df.loc[address_match_condition].iloc[0]
+    address_row_match = fake_data_df.loc[address_match_condition].iloc[-1]
     response_json = json.loads(address_row_match.to_json())
     return dict(response_json)
 
@@ -56,6 +56,6 @@ def mock_business_query(
     business_name = business.names[0]
     business_row_match = fake_data_df.loc[
         business_name.strip() == fake_data_df["name"].str.strip()
-    ].iloc[0]
+    ].iloc[-1]
     response_json = json.loads(business_row_match.to_json())
     return dict(response_json)
